c6312924cbd83e08dcfddef798709f3931459d40
Kévin <kefuret@gmail.com>
2017-10-25 15:39:09 +0200

    :bug: main.js > Fixed problem with navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-  });
-  /* 'use strict';
+    var c, currentScrollTop = 0,
+        navbar = document.getElementById('header');
 
-   var c, currentScrollTop = 0,
-       navbar = $('nav');
-
-   $(window).scroll(function () {
-      var a = $(window).scrollTop();
-      var b = navbar.height();
-
-      currentScrollTop = a;
-
-      if (c < currentScrollTop && a > b + b) {
-        navbar.addClass("scrollUp");
-      } else if (c > currentScrollTop && !(a <= b)) {
-        navbar.removeClass("scrollUp");
-      }
-      c = currentScrollTop;
-  }); */
+    window.addEventListener("scroll", function () {
+        var a = window.scrollY;
+        var b = navbar.clientHeight;
+        currentScrollTop = a;
+        if (c < currentScrollTop && a > b + b) {
+            navbar.classList.add("scrollUp");
+        } else if (c > currentScrollTop && !(a <= b)) {
+            navbar.classList.remove("scrollUp");
+        }
+        c = currentScrollTop;
+    });
+});
