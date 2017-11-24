ab354974b14bcc24573ba0e9e9f63b57d93b0102
MargauxFroidefont <margaux.froidefont@gmail.com>
2017-11-24 18:23:53 +0100

    :lipstick: :construction: Continue animations

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,21 +2,21 @@ export default function(event) {
     let c, currentScrollTop = 0;
     const navbar = document.querySelector('.header');
 
+    console.log("detection js");
+
     window.addEventListener("scroll", function () {
+        console.log("detection scroll");
 
         let a = window.scrollY;
         const b = navbar.clientHeight;
         currentScrollTop = a;
+
         if (c < currentScrollTop && a > b + b) {
             navbar.classList.add("scrollUp");
         } else if (c > currentScrollTop && !(a <= b)) {
             navbar.classList.remove("scrollUp");
         }
+
         c = currentScrollTop;
     });
-
-    setInterval(function() {
-      let test = window.scrollY;
-      console.log(test)
-    }, 500);
 }
---
+++ src/js/popup.js
@@ -0,0 +1,27 @@
+export default function(event) {
+    var buttons = document.getElementsByClassName('popup-button');
+    console.log(buttons);
+    for (var i = 0; i <= buttons.length-1; i++) {
+        console.log(buttons[i]);
+        buttons[i].addEventListener('click', (e) => {
+
+            alert(buttons[i].getAttribute('data-popin'));
+            /*const url = e.target.getAttribute('data-popin') // 'fichier-1.html'
+
+            fetch(url)
+                .then(response => response.text())
+                .then((data) => {
+                    const div = document.createElement('div')
+                    div.innerHTML = data
+                    div.setAttribute('test', true)
+                    div.classList.add('mySuperClass')
+                    document.querySelector('.myclass').appendChild = div
+                })*/
+        })
+    }
+
+    /*close.addEventListerner('click', (e) => {
+        document.querySelector('.test').remove()
+    })*/
+
+}
