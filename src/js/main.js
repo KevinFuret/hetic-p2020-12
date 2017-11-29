87de417906745b579c721d45c9bf7de30a5bb9b8
Kévin <kefuret@gmail.com>
2017-11-29 01:18:54 +0100

    :features: Add video support

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import popup from "./popup.js";
 import Headroom from "headroom.js";
+import youtube from "./youtube.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   popup();
+  youtube();
     const header = document.querySelector(".header");
     const headroom  = new Headroom(header);
     headroom.init();
---
+++ src/js/youtube.js
@@ -0,0 +1,27 @@
+export default () => {
+
+    const youtube = document.querySelectorAll( ".youtube" );
+
+    for (let i = 0; i < youtube.length; i++) {
+
+        const source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+
+        const image = new Image();
+        image.src = source;
+        image.addEventListener( "load", function() {
+            youtube[ i ].appendChild( image );
+        }( i ) );
+
+        youtube[i].addEventListener( "click", function() {
+
+            const iframe = document.createElement( "iframe" );
+            iframe.setAttribute( "frameborder", "0" );
+            iframe.setAttribute( "allowfullscreen", "" );
+            iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1&showinfo=0&controls=0" );
+
+            this.innerHTML = "";
+            this.appendChild( iframe );
+        } );
+    }
+
+}
